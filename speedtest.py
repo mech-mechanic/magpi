dd12bb14f81aa314dde639e784e335c2864c517b
Mike V <mpv@mechdev.io>
2018-03-09 20:04:23 +1100

    latest

--- speedtest.py
+++ speedtest.py
@@ -1,23 +1,3 @@
-# Copyright (c) 2017 Adafruit Industries
-# Author: Tony DiCola & James DeVito
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 import time
 
 import Adafruit_GPIO.SPI as SPI
@@ -45,27 +25,6 @@ SPI_DEVICE = 0
 
 # 128x32 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-
-# 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
-# Note you can change the I2C address by passing an i2c_address parameter like:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3C)
-
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
-
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
 
 # Initialize library.
 disp.begin()
@@ -103,18 +62,37 @@ font = ImageFont.load_default()
 # font = ImageFont.truetype('Minecraftia.ttf', 8)
 
 
-    # Draw a black filled box to clear the image
+# Draw a black filled box to clear the image
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-cmd = "speedtest"
-IP = subprocess.check_output(cmd, shell = True )
+# Begin speedtest code
+from pyspeedtest import SpeedTest
 
-    # Write two lines of text.
+test = SpeedTest()
+host = test.host
+ping = test.ping()
+ping = format(ping, '.2f')
+download = test.download()
+upload = test.upload()
+down = download/1000000
+down = format(down, '.2f')
+up = upload/1000000
+up = format(up, '.2f')
 
-draw.text((x, top),       str(IP),  font=font, fill=255)
+host = host
+ping = "Ping: " + ping + " ms"
+down = "Download: " + down + " Mbps"
+up = "Upload: " + up + " Mbps"
+
+
+# Write four lines of text.
+while True:
+    draw.text((x, top), host,  font=font, fill=255)
+    draw.text((x, top+8), ping, font=font, fill=255)
+    draw.text((x, top+16), down,  font=font, fill=255)
+    draw.text((x, top+25), up,  font=font, fill=255)
 
     # Display image.
-disp.image(image)
-disp.display()
-time.sleep(.1)
+    disp.image(image)
+    disp.display()
+    time.sleep(.1)
